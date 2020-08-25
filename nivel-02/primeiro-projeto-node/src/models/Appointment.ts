46c126c6dc9a3031913b9181cfa925c092e6d32e
Vinnicius Gomes <hello@vinniciusgomes.dev>
2020-08-25 18:44:11 -0300

    Aplicando DTO e repositories

--- nivel-02/primeiro-projeto-node/src/models/Appointment.ts
+++ nivel-02/primeiro-projeto-node/src/models/Appointment.ts
@@ -5,12 +5,11 @@ class Appointment {
   provider: string;
   date: Date;
 
-  constructor(provider: string, date: Date) {
+  constructor({ provider, date }: Omit<Appointment, 'id'>) {
     this.id = uuid();
     this.provider = provider;
     this.date = date;
   }
 }
-
 
 export default Appointment;
--- nivel-02/primeiro-projeto-node/src/routes/appointments.routes.ts
+++ nivel-02/primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,16 +1,23 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
 
-import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointment = appointmentsRepository.all();
+
+  return response.json(appointment);
+});
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
   const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentInSameDate) {
@@ -19,11 +26,12 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
-  appointments.push(appointment);
-
-  return response.json(appointments);
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
