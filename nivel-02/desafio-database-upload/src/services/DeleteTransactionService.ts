cf29a4512c92f251fa20b467e99e869296a09e22
Vinnicius Gomes <hello@vinniciusgomes.dev>
2020-09-04 10:11:38 -0300

    Adicionando desafio de database-upload

---
+++ nivel-02/desafio-database-upload/src/services/DeleteTransactionService.ts
@@ -0,0 +1,20 @@
+import { getCustomRepository, TransactionRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
+import transactionRepository from '../repositories/TransactionsRepository';
+
+class DeleteTransactionService {
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(transactionRepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Unexistent transaction!');
+    }
+    await transactionsRepository.remove(transaction);
+  }
+}
+
+export default DeleteTransactionService;
